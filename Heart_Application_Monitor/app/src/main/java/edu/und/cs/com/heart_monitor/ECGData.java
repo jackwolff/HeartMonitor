f8ea54d2a625af872b7856c554121d727410f874
Jack Wolff <jackwolff9@gmail.com>
2017-02-20 14:51:54 -0600

    QRS Detection updated

--- Heart_Application_Monitor/app/src/main/java/edu/und/cs/com/heart_monitor/ECGData.java
+++ Heart_Application_Monitor/app/src/main/java/edu/und/cs/com/heart_monitor/ECGData.java
@@ -1,6 +1,6 @@
 package edu.und.cs.com.heart_monitor;
 import java.util.ArrayList;
-
+import android.util.Log;
 /**
  * Created by Ryan Mindt on 2/16/2017.
  *
@@ -9,49 +9,60 @@ import java.util.ArrayList;
 
 public class ECGData {
 
-    private ArrayList<Integer> rawList = new ArrayList<Integer>(250);
-    private ArrayList<Integer> LPList = new ArrayList<Integer>(250);
-    private ArrayList<Integer> HPList = new ArrayList<Integer>(250);
+    private static ArrayList<Integer> rawList = new ArrayList<Integer>();
+    private static ArrayList<Integer> LPList = new ArrayList<Integer>();
+    private static ArrayList<Integer> HPList = new ArrayList<Integer>();
 
-    private ArrayList<Integer> timeList = new ArrayList<Integer>(250);
+    private static ArrayList<Integer> timeList = new ArrayList<Integer>();
 
-    public void addPoint(int timeValue, int rawValue) {
+    public static void addPoint(int timeValue, int rawValue) {
         rawList.add(rawValue);
         timeList.add(timeValue);
         lowPass();
         highPass();
     }
 
-    private void lowPass(){
+    public static int getHighPassVal(int x) {
+        int val = 0;
+        try {
+            val = HPList.get(x);
+        }
+        catch(Exception e) {
+            Log.d("ERROR", e.getMessage());
+        }
+        return val;
+    }
+
+    public static int getLowPassVal() {
+        return LPList.get(LPList.size() - 1);
+    }
+
+    private static void lowPass(){
         if(rawList.size() < 12) {
-            LPList.add(rawList.size());
+            LPList.add(rawList.get(rawList.size() - 1));
             return;
         }
 
         int x, x6, x12, y1, y2, y; //x -> past inputs(unfiltered y) y->past outputs (filtered y)
-        x = rawList.get(rawList.size());
-        x6 = rawList.get(rawList.size() - 6);
-        x12 = rawList.get(rawList.size() - 12);
-        y1 = LPList.get(LPList.size());
-        y2 = LPList.get(LPList.size() - 1);
-        y = 2*y1 - y2 +x - 2*x6 + x12;
+        x = rawList.get(rawList.size() - 1);
+        x6 = rawList.get(rawList.size() - 7);
+        x12 = rawList.get(rawList.size() - 13);
+        y1 = LPList.get(LPList.size() - 2);
+        y2 = LPList.get(LPList.size() - 3);
+        y = 2*y1 - y2 + x - 2*x6 + x12;
         LPList.add(y);
     }
-    private void highPass(){
-        if(LPList.size() <32) {
-            HPList.add(LPList.get(LPList.size()));
+    private static void highPass(){
+        if(LPList.size() < 32) {
+            HPList.add(LPList.get(LPList.size() - 1));
             return;
         }
         int x, x16, x32, y1, y; //x-> outputs from lowPass y->outputs from highPass
-        x = LPList.get(LPList.size());
-        x16 = LPList.get(LPList.size() - 16);
-        x32 = LPList.get(LPList.size() - 32);
-        y1 = HPList.get(HPList.size() - 1);
+        x = LPList.get(LPList.size() - 1);
+        x16 = LPList.get(LPList.size() - 17);
+        x32 = LPList.get(LPList.size() - 33);
+        y1 = HPList.get(HPList.size() - 2);
         y = 32*x16 - (y1 + x - x32);
         HPList.add(y);
     }
-
-
-
-
 }

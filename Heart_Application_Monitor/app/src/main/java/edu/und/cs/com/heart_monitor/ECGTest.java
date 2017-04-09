9d2cfe611c22b037a7e392479de0c89e57e4e589
Jack Wolff <jackwolff9@gmail.com>
2017-04-09 18:23:44 -0500

    Updated ECG, it is working without the filter. Filter was throwing an out of bounds exception.

--- Heart_Application_Monitor/app/src/main/java/edu/und/cs/com/heart_monitor/ECGTest.java
+++ Heart_Application_Monitor/app/src/main/java/edu/und/cs/com/heart_monitor/ECGTest.java
@@ -35,7 +35,7 @@ public class ECGTest extends RoboActivity implements View.OnClickListener {
     private LineGraphSeries fileSeries;
     private GraphView ecgGraphView;
 
-    AsyncTask task;
+    AsyncTask task = null;
     private boolean isAsyncTaskCancelled = false;
 
     ECGFilter data;
@@ -115,12 +115,9 @@ public class ECGTest extends RoboActivity implements View.OnClickListener {
      */
     private void onFileButton() {
 
-        try {
-            AssetManager mnger = getAssets();
-            InputStream stream = mnger.open("samples/Sample1-Filtered.txt");
-        }
-        catch(Exception e) {
-            Log.d("TAG", e.getMessage());
+        //If we have a task running, return
+        if(task != null){
+            return;
         }
 
         //Get the AssetManager and get all the sample files
@@ -131,6 +128,7 @@ public class ECGTest extends RoboActivity implements View.OnClickListener {
             //Create a dialog to select a file to read from
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Pick Sample");
+            //Once a file has been chosen, close the dialog and start the task
             builder.setItems(samples, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int index) {
                     //Close the dialog
@@ -202,7 +200,7 @@ public class ECGTest extends RoboActivity implements View.OnClickListener {
                     //Plot the points
                     publishProgress();
                     try {
-                        Thread.sleep(5);
+                        Thread.sleep(1);
                         Log.d("WAIT", "Waiting...");
                     }
                     catch(Exception e) {
@@ -254,7 +252,7 @@ public class ECGTest extends RoboActivity implements View.OnClickListener {
                         new DataPoint[] {
                             new DataPoint(cur_x, file[cur_x % sample])
                         });
-                ecgGraphView.addSeries(point);
+                //ecgGraphView.addSeries(point);
             }
         }
     }
